5a57bd83ed7e6c8628f6a62dc27fddb4fb745a3d
iamarirosenthal <iamarirosenthal@outlook.com>
2017-11-25 11:42:43 -0500

    fist commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom-Swiss Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('A1 Steak Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno BlackBean Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Chicken Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Blue Burger', true);
